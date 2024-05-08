ae0546949c79fcf2f030924dec921870dfcdb1aa
Arthur Van Ginderachter <arthur@vanginderachter.be>
2024-05-08 10:30:19 +0200

    vorige installatie guestediitons alles naar stdout dev null om crash te vermijden

--- Shared-Folder/guestupdate.sh
+++ Shared-Folder/guestupdate.sh
@@ -17,6 +17,8 @@ echo "Latest version: ${VBOX_VERSION}"
 # Compare the current installed version with the latest available version
 if [ "${VBOX_VERSION}" != "${INSTALLED_VERSION%%r*}" ]; then
     # Update package list and install dependencies
+    echo "------------------------verwidjeren van eventuel eoudere versies en installatie voorbereiden....-------------------------------------"
+
     echo 'osboxes.org' | sudo -S DEBIAN_FRONTEND=noninteractive apt purge virtualbox-guest-utils virtualbox-guest-x11 -y >/dev/null
     echo 'osboxes.org' | sudo -S DEBIAN_FRONTEND=noninteractive apt autoremove -y >/dev/null
     echo 'osboxes.org' | sudo -S DEBIAN_FRONTEND=noninteractive apt update -y >/dev/null
@@ -26,17 +28,18 @@ if [ "${VBOX_VERSION}" != "${INSTALLED_VERSION%%r*}" ]; then
     tmp_dir=$(mktemp -d)
 
     # Download VirtualBox Guest Additions ISO
-    echo "downloaden van VBoxGuestAdditions_$VBOX_VERSION.iso"
+    echo "---------------------downloaden van VBoxGuestAdditions_$VBOX_VERSION.iso-----------------------------"
     wget -P $tmp_dir https://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso >/dev/null
     # Mount the ISO
+    echo "--------------------------------mounten van VBoxGuestAdditions_$VBOX_VERSION.iso---------------------------------------------------"
     echo 'osboxes.org' | sudo -S mount -o loop $tmp_dir/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 
-    echo "-------------Remosboxudo -S sh /mnt/VBoxLinuxAdditions.run uninstall"
+    
     
     # Run the installer script with automatic "yes" responses to prompts
-    echo "--------Installing the new version of VirtualBox Guest Additions-------------------"
+    echo "--------Installing the new version of VirtualBox Guest Additions $VBOX_VERSION-------------------"
     yes | sudo -S /mnt/VBoxLinuxAdditions.run >/dev/null
-    echo "-----Updated to version: $VBOX_VERSION-----------"
+    echo "------------------Updated from $INSTALLED_VERSION version: $VBOX_VERSION------------------------------"
     # Clean up
     echo 'osboxes.org' | sudo -S  umount /mnt
     rm -rf $tmp_dir >/dev/null
